4b5ee7a8a2a81bb4e785b7fb62ae4dee74fb7c5b
lololll0o <lololll0o0808@gmail.com>
2025-07-06 21:50:59 +0900

    chore: Switch to HashRouter for GitHub Pages compatibility (GitHub Pages 배포를 위한 HashRouter 적용)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import RecommendationPage from "./pages/RecommendationPage";
 import PlaylistPage from "./pages/PlaylistPage";
